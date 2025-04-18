8fded48c12395e70f46ef040b28f58ff8ddeffd1
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-04-18 21:21:11 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.strapi_ec2.public_ip
+  description = "Public IP of the deployed EC2 instance"
+  value       = aws_instance.strapi.public_ip
 }
