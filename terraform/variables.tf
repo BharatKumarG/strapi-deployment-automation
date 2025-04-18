b8e1ecd909e7da8a1333cec3e22838574ec7751b
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-04-18 19:56:03 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,32 +1,4 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-  default     = "us-east-1"  # Adjust to your desired region
-}
-
-variable "ecr_registry" {
-  description = "The ECR registry URL"
-  type        = string
-}
-
-variable "ecr_repository" {
-  description = "The ECR repository name"
-  type        = string
-}
-
-variable "image_tag" {
-  description = "The tag of the Docker image"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "The EC2 instance type"
-  type        = string
-  default     = "t2.medium"
-}
-
-variable "ami_name_filter" {
-  description = "The filter for the AMI name"
-  type        = string
-  default     = "amzn2-ami-hvm-*-x86_64-gp2"
+# provider.tf
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
 }
