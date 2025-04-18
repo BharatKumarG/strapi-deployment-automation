49aa83e5520f8051d26499123f05e95865ce7b65
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-04-18 21:13:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,11 @@
-# main.tf
-resource "aws_instance" "strapi" {
-  ami                    = "ami-0e449927258d45bc4" # Use correct AMI ID for your region
-  instance_type          = "t2.medium"
-  key_name               = "bharath"
-  associate_public_ip_address = true
+provider "aws" {
+  region = var.aws_region
+}
 
-  # Attach security group (optional)
-  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+data "aws_vpc" "default" {
+  default = true
+}
 
-  tags = {
-    Name = "Strapi-EC2"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              # Update packages
-              yum update -y
-
-              # Install Docker
-              amazon-linux-extras install docker -y
-              service docker start
-              usermod -a -G docker ec2-user
-
-              # Install AWS CLI v2
-              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-              unzip awscliv2.zip
-              sudo ./aws/install
-
-              # Login to ECR
-              aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${var.ecr_registry}
-
-              # Pull and run the Docker image from ECR
-              docker pull ${var.ecr_image}
-              docker run -d -p 80:1337 ${var.ecr_image}
-              EOF
 resource "aws_security_group" "strapi_sg" {
   name        = "strapi-sg"
   description = "Allow HTTP and SSH"
@@ -63,5 +35,32 @@ resource "aws_security_group" "strapi_sg" {
   }
 }
 
+resource "aws_instance" "strapi" {
+  ami                         = "ami-0e449927258d45bc4" # ✅ Saved AMI for Amazon Linux 2 (ap-south-1)
+  instance_type               = "t2.medium"
+  key_name                    = "bharath"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.strapi_sg.id]
 
+  tags = {
+    Name = "Strapi-EC2"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              usermod -a -G docker ec2-user
+
+              # Install AWS CLI v2
+              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              unzip awscliv2.zip
+              sudo ./aws/install
+
+              # Login to ECR and run your Strapi container
+              aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${var.ecr_registry}
+              docker pull ${var.ecr_image}
+              docker run -d -p 80:1337 ${var.ecr_image}
+              EOF
 }
