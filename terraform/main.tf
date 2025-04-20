e50c4caba77ea925913a8cc1d1a140b742b4e905
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-04-20 21:28:01 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,6 +48,14 @@ resource "aws_security_group" "strapi_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+  from_port   = 1337
+  to_port     = 1337
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  description = "Allow Strapi"
+}
+
 
   egress {
     from_port   = 0
