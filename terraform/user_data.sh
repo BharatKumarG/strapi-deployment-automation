fcb2dd387bd423a70855c5266b4940e94d0598d8
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-04-21 12:31:00 +0530

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-# Update and install Docker
+
+# Update and install required packages
 yum update -y
-yum install -y docker
+yum install -y docker jq awscli
 service docker start
 usermod -a -G docker ec2-user
 
-# Log in to AWS ECR
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin <aws_account_id>.dkr.ecr.us-east-1.amazonaws.com
+# ECR login
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 118273046134.dkr.ecr.us-east-1.amazonaws.com
 
-# Pull the Strapi image using the image tag passed from GitHub Actions
-docker pull <aws_account_id>.dkr.ecr.us-east-1.amazonaws.com/strapi-repo:${image_tag}
+# Get the latest image tag from ECR
+latest_tag=$(aws ecr describe-images \
+  --repository-name gbk-strapi-app \
+  --region us-east-1 \
+  --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' \
+  --output text)
 
-# Run the Strapi container
-docker run -d -p 1337:1337 <aws_account_id>.dkr.ecr.us-east-1.amazonaws.com/strapi-repo:${image_tag}
+# Pull the image with the latest tag
+docker pull 118273046134.dkr.ecr.us-east-1.amazonaws.com/gbk-strapi-app:$latest_tag
+
+# Run the container
+docker run -d -p 1337:1337 118273046134.dkr.ecr.us-east-1.amazonaws.com/gbk-strapi-app:$latest_tag
