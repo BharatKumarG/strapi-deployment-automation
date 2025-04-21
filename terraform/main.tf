8e2edfcdc1c77a4ba4e6fe12b9f63eb9e70ec1c7
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-04-21 12:14:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,40 +1,28 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Changed region to us-east-1
 }
 
-resource "aws_instance" "strapi" {
-  ami                    = "ami-0e449927258d45bc4" # Replace with a valid AMI ID for your region
-  instance_type          = "t2.medium"
-  key_name               = "bharath"               # Replace with your key pair name
-  associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
-              usermod -a -G docker ec2-user
-              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-              unzip awscliv2.zip
-              sudo ./aws/install
-              aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 118273046134.dkr.ecr.us-east-1.amazonaws.com
-              docker pull 118273046134.dkr.ecr.us-east-1.amazonaws.com/gbk-strapi-app:latest
-              docker run -d -p 80:1337 118273046134.dkr.ecr.us-east-1.amazonaws.com/gbk-strapi-app:latest
-              EOF
-
-  tags = {
-    Name = "Strapi-EC2-GBK"
-  }
+# Create VPC
+resource "aws_vpc" "strapi_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 }
 
+# Create Security Group for EC2 instance with updated name
 resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-sg-GBKGy"
-  description = "Allow HTTP and SSH"
-  vpc_id      = data.aws_vpc.default.id
+  name        = "strapi.app.gbkg"  # Changed to strapi.app.gbkg
+  description = "Security group for Strapi EC2 instance"
+  vpc_id      = aws_vpc.strapi_vpc.id
 
   ingress {
-    description = "SSH"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -42,20 +30,11 @@ resource "aws_security_group" "strapi_sg" {
   }
 
   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 1337
+    to_port     = 1337
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Port 1337 open for Strapi application access
   }
-  ingress {
-  from_port   = 1337
-  to_port     = 1337
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  description = "Allow Strapi"
-}
-
 
   egress {
     from_port   = 0
@@ -65,6 +44,49 @@ resource "aws_security_group" "strapi_sg" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
+# Create EC2 instance
+resource "aws_instance" "strapi_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Choose an Amazon Linux AMI
+  instance_type = "t2.micro"
+  key_name      = "bharath"  # Changed to key name 'bharath'
+  security_groups = [aws_security_group.strapi_sg.name]
+  subnet_id     = aws_subnet.strapi_subnet.id
+
+  user_data = data.template_file.user_data.rendered
+
+  tags = {
+    Name = "Strapi EC2 Instance"
+  }
+}
+
+# Create a subnet for the EC2 instance
+resource "aws_subnet" "strapi_subnet" {
+  vpc_id                  = aws_vpc.strapi_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"  # Changed to us-east-1
+  map_public_ip_on_launch = true
+}
+
+# ECR repository for storing the Strapi Docker image
+resource "aws_ecr_repository" "strapi_repo" {
+  name = "strapi-repo"
+}
+
+# Get the Docker image tag from GitHub actions output (e.g., GitHub SHA)
+variable "image_tag" {
+  description = "The tag of the Docker image to be deployed"
+  type        = string
+}
+
+# Use the image tag in user data to pull the Docker image
+data "template_file" "user_data" {
+  template = file("terraform/user_data.sh")
+
+  vars = {
+    image_tag = var.image_tag
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.strapi_instance.public_ip
 }
