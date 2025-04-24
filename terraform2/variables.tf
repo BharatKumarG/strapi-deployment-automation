95812dd84b9266a44654b5db65b234f4edb232a1
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-04-24 15:56:46 +0530

    Update variables.tf

--- terraform2/variables.tf
+++ terraform2/variables.tf
@@ -27,24 +27,29 @@ variable "region" {
 variable "app_keys" {
   type        = string
   description = "Application keys for Strapi"
+  sensitive   = true
 }
 
 variable "api_token_salt" {
   type        = string
   description = "API token salt for Strapi"
+  sensitive   = true
 }
 
 variable "admin_jwt_secret" {
   type        = string
   description = "Admin JWT secret for Strapi"
+  sensitive   = true
 }
 
 variable "transfer_token_salt" {
   type        = string
   description = "Transfer token salt for Strapi"
+  sensitive   = true
 }
 
 variable "jwt_secret" {
   type        = string
   description = "JWT secret for Strapi"
+  sensitive   = true
 }
