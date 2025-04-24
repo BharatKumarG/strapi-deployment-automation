fa708ad773eda0bd1623f1089983c225ed9f4dc3
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-04-24 16:42:19 +0530

    Update variables.tf

--- terraform2/variables.tf
+++ terraform2/variables.tf
@@ -1,16 +1,3 @@
-# Variable for AWS region
- variable "region" {
-    description = "AWS region"
-    default     = "us-east-1"
-  }
-  
-  variable "image_uri" {
-    description = "The URI of the Docker image in ECR"
-    type        = string
-    default     = "118273046134.dkr.ecr.us-east-1.amazonaws.com/gbk-strapi-app:latest"
-  }
-
-
 variable "existing_vpc_id" {
   description = "ID of the existing VPC"
   default     = "vpc-0d255f5b20be72ef6"
