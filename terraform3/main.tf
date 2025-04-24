8ac32a1971ab5600bc56cfdc998459209cabf10b
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-04-24 11:53:07 +0530

    Update main.tf

--- terraform3/main.tf
+++ terraform3/main.tf
@@ -1,16 +1,26 @@
+# provider configuration
 provider "aws" {
   region = "us-east-1"
 }
 
+# Declare region as a variable
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+# Declare the Strapi image as a variable
 variable "strapi_image" {
   default = "118273046134.dkr.ecr.us-east-1.amazonaws.com/gbk-strapi-app:latest"
 }
 
+# CloudWatch Log Group for ECS
 resource "aws_cloudwatch_log_group" "strapi" {
   name              = "/ecs/strapi"
   retention_in_days = 7
 }
 
+# Create VPC for Strapi
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -21,6 +31,7 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Create Public Subnet
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -32,6 +43,7 @@ resource "aws_subnet" "public" {
   }
 }
 
+# Create Internet Gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 
@@ -40,6 +52,7 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+# Create Route Table for Public Subnet
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -53,11 +66,13 @@ resource "aws_route_table" "public" {
   }
 }
 
+# Route Table Association for Public Subnet
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
 
+# Create Security Group for Strapi
 resource "aws_security_group" "strapi_sg" {
   name        = "strapi-sg"
   description = "Allow 1337 and HTTP access"
@@ -89,10 +104,12 @@ resource "aws_security_group" "strapi_sg" {
   }
 }
 
+# ECS Cluster for Strapi
 resource "aws_ecs_cluster" "strapi" {
   name = "strapi-cluster"
 }
 
+# ECS Task Definition
 resource "aws_ecs_task_definition" "strapi" {
   family                   = "strapi-task"
   network_mode             = "awsvpc"
@@ -102,26 +119,25 @@ resource "aws_ecs_task_definition" "strapi" {
   execution_role_arn       = "arn:aws:iam::118273046134:role/ecsTaskExecutionRole1"
   task_role_arn            = "arn:aws:iam::118273046134:role/ecsTaskExecutionRole1"
 
-  container_definitions = jsonencode([
-    {
-      name      = "strapi",
-      image     = var.strapi_image,
-      portMappings = [{
-        containerPort = 1337,
-        protocol      = "tcp"
-      }],
-      logConfiguration = {
-        logDriver = "awslogs",
-        options = {
-          awslogs-group         = "/ecs/strapi",
-          awslogs-region        = "us-east-1",
-          awslogs-stream-prefix = "ecs"
-        }
+  container_definitions = jsonencode([{
+    name      = "strapi",
+    image     = var.strapi_image,
+    portMappings = [{
+      containerPort = 1337,
+      protocol      = "tcp"
+    }],
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        awslogs-group         = "/ecs/strapi",
+        awslogs-region        = var.region,
+        awslogs-stream-prefix = "ecs"
       }
     }
-  ])
+  }])
 }
 
+# ECS Service for Strapi
 resource "aws_ecs_service" "strapi" {
   name            = "strapi-service"
   cluster         = aws_ecs_cluster.strapi.id
@@ -136,6 +152,7 @@ resource "aws_ecs_service" "strapi" {
   }
 }
 
+# Output the Strapi URL
 output "strapi_url" {
   value = "http://${aws_ecs_service.strapi.name}.ecs.${var.region}.amazonaws.com:1337"
 }
