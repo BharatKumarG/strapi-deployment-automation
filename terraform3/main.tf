80832098afecec1b3d5a9e6f60007027fbdfa213
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-04-30 13:56:58 +0530

    Update main.tf

--- terraform3/main.tf
+++ terraform3/main.tf
@@ -46,7 +46,7 @@ resource "aws_route_table_association" "b" {
 resource "aws_security_group" "strapi_sg" {
   name        = "strapi-sg"
   vpc_id      = aws_vpc.main.id
-  description = "Allow HTTP inbound traffic"
+  description = "Allow HTTP traffic"
 
   ingress {
     from_port   = 80
@@ -73,16 +73,16 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Action    = "sts:AssumeRole",
       Effect    = "Allow",
       Principal = {
         Service = "ecs-tasks.amazonaws.com"
-      }
+      },
+      Action = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
@@ -97,8 +97,7 @@ resource "aws_ecs_task_definition" "strapi" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -128,16 +127,16 @@ resource "aws_lb" "strapi" {
   name               = "strapi-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.strapi_sg.id]
   subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  security_groups    = [aws_security_group.strapi_sg.id]
 }
 
 resource "aws_lb_target_group" "strapi" {
   name        = "strapi-tg"
   port        = 1337
   protocol    = "HTTP"
-  target_type = "ip"
   vpc_id      = aws_vpc.main.id
+  target_type = "ip"
 
   health_check {
     path                = "/"
@@ -165,8 +164,8 @@ resource "aws_ecs_service" "strapi" {
   name            = "strapi-service"
   cluster         = aws_ecs_cluster.strapi.id
   task_definition = aws_ecs_task_definition.strapi.arn
-  launch_type     = "FARGATE"
   desired_count   = 1
+  launch_type     = "FARGATE"
 
   network_configuration {
     subnets         = [aws_subnet.public_a.id, aws_subnet.public_b.id]
@@ -180,7 +179,5 @@ resource "aws_ecs_service" "strapi" {
     container_port   = 1337
   }
 
-  depends_on = [
-    aws_lb_listener.front_end
-  ]
+  depends_on = [aws_lb_listener.front_end]
 }
